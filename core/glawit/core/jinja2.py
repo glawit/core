213f8c1119691ee3cd0c275cfeab36949211944c
David Joaquín Shourabi Porcel <david@djsp.eu>
2020-06-24 02:17:13 +0200

    FIXME

--- core/glawit/core/jinja2.py
+++ core/glawit/core/jinja2.py
@@ -2,25 +2,24 @@ import importlib.resources
 
 import jinja2
 
-loader = jinja2.PackageLoader(
-    'glawit.core.data',
-    package_path='data/jinja2',
-)
+data_package = 'glawit.core.data.jinja2'
 
-package = 'glawit.core.data.jinja2'
+loader = jinja2.FunctionLoader(
+    load_template,
+)
 
 
 def load_template(name):
     exists = importlib.resources.is_resource(
         name=name,
-        package=package,
+        package=data_package,
     )
 
     assert exists
 
     content = importlib.resources.read_text(
         encoding='utf-8',
-        package=package,
+        package=data_package,
         resource=name,
     )
 
